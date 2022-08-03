a680360bfecf2dc1949cd972bd7f88eaadd49cd6
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-03 19:07:11 +0900

    typeof

--- 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
+++ 04_JavaScriptの基礎を固める/020_変数の定義と型/start/main.js
@@ -1,4 +1,17 @@
 // var, let , const
-const Name = "tom";
+let Name = "tom";
 Name = "tim";
 console.log('hello' + Name);
+
+
+//Number, String, Boolean, Undefined, Null, Symbol
+
+let variable = 'str';
+console.log(typeof variable);
+variable = 12;
+console.log(typeof variable);
+variable = false;
+console.log(typeof variable);
+variable = undefined;
+console.log(typeof variable);
+
