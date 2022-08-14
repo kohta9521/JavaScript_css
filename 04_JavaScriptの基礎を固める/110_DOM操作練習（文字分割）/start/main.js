c948b9e909146d6732caf2360fd42b214fe25a01
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-14 14:08:18 +0900

    concatStr

--- 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
+++ 04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const el = document.querySelector('.animate-title');
-    console.log(el.innerHTML.trim());
+    const str = el.innerHTML.trim();
+    let concatStr = '';
+
+
+    for(let c of str) {
+        concatStr += `<span class="char">${c}</span>`;
+    }
+    console.log(concatStr);
 })
