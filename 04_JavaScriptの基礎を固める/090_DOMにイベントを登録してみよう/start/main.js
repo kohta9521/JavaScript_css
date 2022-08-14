e28a6e1e24bf1a005d6e41922999010683080cca
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-14 13:29:04 +0900

    複数登録

--- 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -1,6 +1,12 @@
 const btn = document.querySelector('#btn');
+const h1 = document.querySelector('h1');
 
-const hello = function() {
-    alert('hello');
+function changeColor() {
+    h1.style.color = 'red';
 }
-btn.addEventListener('click', hello);
+function changeBgColor() {
+    h1.style.backgroundColor = 'green';
+}
+btn.addEventListener('click', changeColor);
+btn.addEventListener('click', changeBgColor);
+// btn.removeEventListener('click', hello);
