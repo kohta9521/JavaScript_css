a0012d93f20dd9002f00890a982f727e7526d3ee
河内　光太 <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-17 15:49:46 +0900

    for of  for in

--- 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
+++ 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
@@ -1,5 +1,11 @@
-const arry = [1, 2, 3, 4, 5, 6];
+const arry = [1, 2, 3, 4, 5, 6]
 
-for (let i of arry) {
-    console.log(i);
+for(let i in arry) {
+    console.log(i, arry[i]);
+}
+
+
+//vはvalue
+for(let v of arry) {
+    console.log(v);
 }
