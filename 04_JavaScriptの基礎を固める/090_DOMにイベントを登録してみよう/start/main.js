2a2092a315d1554a93ce65199abe6dc8a0a6b07f
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-29 12:16:13 +0900

    addEventListenor

--- 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -1,8 +1,12 @@
-const btn = document.querySlecter('#btn');
+const btn = document.querySelector('#btn');
+const h1 = document.querySelector('h1');
 
-function hello() {
-    alert('hello');
-}
+function changeColor() {
+    h1.style.color = "red";
+};
+function changeBackGroundColor() {
+    h1.style.backgroundColor = "green";
+};
 
-btn.addEventListener('clock', hello);
-btn.removeEventListener('mouseenter', hello);
+btn.addEventListener('click', changeColor);
+btn.addEventListener('click', changeBackGroundColor);
