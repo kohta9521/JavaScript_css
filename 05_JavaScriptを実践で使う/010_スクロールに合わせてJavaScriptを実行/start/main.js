dcf7371ac6bbc032856d7bbf1f3758a664a348c4
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-29 12:42:28 +0900

    observer

--- 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
+++ 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -3,11 +3,18 @@ const cb = function(entries, observer) {
     entries.forEach(entry => {
         if(entry.isIntersecting) {
                 console.log("inview");
+                entry.target.classList.add('inview');
+                // observer.unobserve(entry.target);
         } else {
                 console.log("outview");
         }
     });
     // alert('intersecting');
+}
+const options = {
+    root: null, 
+    rootMargin: "0px",
+    threshold:  0
 }
 const io = new IntersectionObserver(cb);
 io.observe(child);
