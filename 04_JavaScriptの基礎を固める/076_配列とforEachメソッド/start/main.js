5aa0731c0252065a80af30f93de607f9138eb743
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-08-29 11:23:59 +0900

    コメント

--- 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
@@ -1,5 +1,13 @@
 const arry = [1,  2, 3, 4, 5];
 
-arry.forEach(function(val, i, ary) {
-    console.log(val, i, ary);
+arry.forEach(function(val) {
+    console.log(val);
 });
+
+arry.forEach(v => console.log(v));
+
+
+for(let i  = 0; i < arry.length; i ++) {
+    const v = arry[i];
+    console.log(v);
+}
